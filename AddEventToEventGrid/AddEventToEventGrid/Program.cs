27be164cd1a1d61d925eeb1baec41e71e4c8cb06
Jakub Hromada <extHromada@dr-max.global>
2024-02-29 11:37:30 +0100

    initial commit

---
+++ AddEventToEventGrid/AddEventToEventGrid/Program.cs
@@ -0,0 +1,27 @@
+﻿using AddEventToEventGrid;
+using Azure;
+using Azure.Messaging;
+using Azure.Messaging.EventGrid;
+
+var client = new EventGridPublisherClient(
+    new Uri("{add event grid url}/api/events"),
+    new AzureKeyCredential("")); // event grid key form Azure portal
+
+var model = new TeamsNotification
+{
+    Subject = "",
+    Timestamp = DateTime.Now,
+    Importance = "",
+    Message = "",
+    Details = "",
+    Country = "",
+    ExecutingUser = "",
+    Action = "",
+    Source = "",
+    Url = "",
+};
+
+// CloudEvent with custom model serialized to JSON
+var cloudEvent = new CloudEvent("{add event grid topic}","{add event type}", model);
+
+client.SendEvent(cloudEvent);
---
+++ AddEventToEventGrid/AddEventToEventGrid/TeamsNotification.cs
@@ -0,0 +1,15 @@
+﻿namespace AddEventToEventGrid;
+
+public class TeamsNotification
+{
+    public string? Subject { get; set; }
+    public DateTime? Timestamp { get; set; }
+    public string? Importance { get; set; }
+    public string? Message { get; set; }
+    public string? Details { get; set; }
+    public string? Source { get; set; }
+    public string? Country { get; set; }
+    public string? Url { get; set; }
+    public string? ExecutingUser { get; set; }
+    public string? Action { get; set; }
+}
