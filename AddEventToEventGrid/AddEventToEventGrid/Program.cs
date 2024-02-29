416ea1c489394ebd0c1c27f82c0d87d51f30be06
Jakub Hromada <>
2024-02-29 18:06:45 +0100

    add json serializer

--- AddEventToEventGrid/AddEventToEventGrid/Program.cs
+++ AddEventToEventGrid/AddEventToEventGrid/Program.cs
@@ -1,7 +1,9 @@
 ﻿using AddEventToEventGrid;
 using Azure;
+using Azure.Core.Serialization;
 using Azure.Messaging;
 using Azure.Messaging.EventGrid;
+using System.Text.Json;
 
 var client = new EventGridPublisherClient(
     new Uri("{add event grid url}/api/events"),
@@ -9,19 +11,29 @@ var client = new EventGridPublisherClient(
 
 var model = new TeamsNotification
 {
-    Subject = "",
+    Subject = "test subject",
     Timestamp = DateTime.Now,
-    Importance = "",
-    Message = "",
-    Details = "",
-    Country = "",
-    ExecutingUser = "",
-    Action = "",
-    Source = "",
-    Url = "",
+    Importance = "Low",
+    Message = "Test message",
+    Details = "test details",
+    Country = "test country",
+    ExecutingUser = "test executing user",
+    Action = "test action",
+    Source = "test source",
+    Url = "test url",
 };
 
+var jsonSerializer = new JsonObjectSerializer(
+    new JsonSerializerOptions()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    });
+
 // CloudEvent with custom model serialized to JSON
-var cloudEvent = new CloudEvent("{add event grid topic}","{add event type}", model);
+var cloudEvent = new CloudEvent(
+    "",
+    "",
+    jsonSerializer.Serialize(model),
+    "application/json");
 
 client.SendEvent(cloudEvent);
