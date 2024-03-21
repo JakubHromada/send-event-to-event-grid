3bc1a398822979817523a964856f1e5c2839f5ae
Jakub Hromada <extHromada@dr-max.global>
2024-03-21 11:09:15 +0100

    update

--- AddEventToEventGrid/AddEventToEventGrid/Program.cs
+++ AddEventToEventGrid/AddEventToEventGrid/Program.cs
@@ -9,18 +9,10 @@ var client = new EventGridPublisherClient(
     new Uri("{add event grid url}/api/events"),
     new AzureKeyCredential("")); // event grid key form Azure portal
 
-var model = new TeamsNotification
+var model = new SessionMessage
 {
-    Subject = "test subject",
+    JobStatus = "succeeded",
     Timestamp = DateTime.Now,
-    Importance = "Low",
-    Message = "Test message",
-    Details = "test details",
-    Country = "test country",
-    ExecutingUser = "test executing user",
-    Action = "test action",
-    Source = "test source",
-    Url = "test url",
 };
 
 var jsonSerializerOptions = new JsonSerializerOptions()
---
+++ AddEventToEventGrid/AddEventToEventGrid/SessionMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace AddEventToEventGrid;
+
+public class SessionMessage
+{
+    public DateTime? Timestamp { get; set; }
+    public string? JobStatus { get; set; }
+}
